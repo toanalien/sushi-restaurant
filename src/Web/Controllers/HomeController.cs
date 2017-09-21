f29b7508b82a71047dd4e9a9f4307e2d4e67a7ad
LinhNguyen0611 <annienguyen.it@gmail.com>
2017-09-21 23:05:44 +0700

    Init commit with DB structủe

---
+++ src/Web/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Data.Model.Entities;
+using Data.Model.ViewModels;
+using Data.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : BaseController
+    {
+        IAspNetUserService _userService;
+
+        public HomeController(IAspNetUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public JsonResult Index()
+        {
+            var db = new Entities();
+            Mapper.Initialize(cfg=>cfg.CreateMap<AspNetUser, AspNetUserViewModel>());
+
+
+            var data = db.AspNetUsers.ProjectTo<AspNetUserViewModel>().ToList() ;
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
