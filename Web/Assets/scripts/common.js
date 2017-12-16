4b7ba06f286228205b6f8908f344e59ff9b46b3a
Phathdt <phathdt.leviateam@gmail.com>
2017-12-17 02:49:54 +0700

    change search input and clear display when change category

--- Web/Assets/scripts/common.js
+++ Web/Assets/scripts/common.js
@@ -12,6 +12,8 @@ $(document).ready(function() {
 });
 
 function GetSubCategories(id) {
+  $('.dishes').html('<h2>No data</h2>')
+
   category = $(`.btn-category#${id}`)
   $.ajax({
     type: 'GET',
@@ -27,19 +29,25 @@ function renderSubCategory() {
   list_item = ''
   if (SubCategories.length) {
     SubCategories.map(function(item) {
-      list_item += `<button class='btn btn-primary btn-sub-category' onclick="GetDishes(${item.ID})" type="button">${item.Name}</button>`;
+      list_item += `<button
+                      class='btn btn-primary btn-sub-category'
+                      onclick="GetDishes(${item.ID})"
+                      type="button">
+                      ${item.Name}
+                    </button>`;
     });
   } else {
     list_item = '<h2>No data</h2>'
   }
 
   $('.sub-categories').html(list_item)
+  $('.btn-sub-category').first().click()
 }
 
 // lay danh sach dish cua sub category id = id
 function GetDishes(id) {
   if (SubCategoriesID.includes(id)) {
-    renderDish(id)
+    renderDishes(id)
     // khong lam gi het
   } else {
     $.ajax({
@@ -48,7 +56,7 @@ function GetDishes(id) {
       success: function (data) {
         Dishes = [ ...Dishes, ...data ]
         SubCategoriesID.push(id)
-        renderDish(id)
+        renderDishes(id)
       }
     });
   }
@@ -56,17 +64,26 @@ function GetDishes(id) {
 }
 
 // render danh sach Dish thuoc Subcategory id = id
-function renderDish(id) {
-  dishesFiltered = Dishes.filter( dish => dish.SubCategoryID == id)
-  console.log(dishesFiltered)
+function renderDishes(id, isSearch = false) {
   list_item = ''
-  if (dishesFiltered.length) {
-    dishesFiltered.map(function(item) {
-     list_item += `<p>${item.Name}</p>`
-    });
+
+  if (isSearch) {
+
   } else {
-    list_item = '<h2>No data</h2>'
+    dishesFiltered = Dishes.filter( dish => dish.SubCategoryID == id)
+    if (dishesFiltered.length) {
+      dishesFiltered.map(function(item) {
+        // list_item += <p>hello</p>
+        list_item += `<p>${item.Name}</p>`
+      });
+    } else {
+      list_item = '<h2>No data</h2>'
+    }
   }
 
   $('.dishes').html(list_item)
+}
+
+function renderDish(dish) {
+
 }
