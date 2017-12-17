9db7d718e60dfc1ce21eab9f46b763bdf0c1644a
Phathdt <phathdt.leviateam@gmail.com>
2017-12-17 15:31:02 +0700

    add RenderOrderItems and button add, minus and remove

--- Web/Assets/scripts/common.js
+++ Web/Assets/scripts/common.js
@@ -3,6 +3,22 @@ Dishes = []
 CategoriesID = []
 SubCategoriesID = []
 searchedDishes = []
+newOrderItems = []
+
+Order = {
+  Id: null,
+  SubTotal: null,
+  PromotionDiscount: null,
+  ClassDiscount: null,
+  Total: null,
+  Status: null,
+  CustomerID: null,
+  EmployeeID: null,
+  TableID: null,
+  Note: null
+}
+
+OrderItem = []
 
 $(document).ready(function() {
   $(".search-dishes").keyup(function(){
@@ -15,6 +31,8 @@ $(document).ready(function() {
   setTimeout(function(){
     $('.btn-sub-category').first().click()
   }, 1000);
+
+  renderOrderItems()
 });
 
 function GetSubCategories(id) {
@@ -82,14 +100,14 @@ function renderDishes(id, isSearch = false) {
   if (isSearch) {
     searchedDishes.map(function(item) {
       // list_item += <p>hello</p>
-      list_item += `<p>${item.Name}</p>`
+      list_item += renderDish(item)
     });
   } else {
     dishesFiltered = Dishes.filter( dish => dish.SubCategoryID == id)
     if (dishesFiltered.length) {
       dishesFiltered.map(function(item) {
         // list_item += <p>hello</p>
-        list_item += `<p>${item.Name}</p>`
+        list_item += renderDish(item)
       });
     } else {
       list_item = '<h2>No data</h2>'
@@ -97,10 +115,32 @@ function renderDishes(id, isSearch = false) {
   }
 
   $('.dishes').html(list_item)
+  $(".dish").each(function() {
+    $(this).click(function() {
+      AddOrderItem($(this))
+    });
+  });
 }
 
 function renderDish(dish) {
-
+  content = `<div class="col-lg-4 col-md-6 mb-4">
+              <div
+                class="card h-100 dish"
+                id="${dish.ID}">
+                  <img
+                    class="card-img-top"
+                    src="/Content/uploads/${dish.Image}"
+                    alt="">
+                <div class="card-body">
+                  <h4 class="card-title">
+                    ${dish.Name}
+                  </h4>
+                  <h5>${dish.Price} vnd</h5>
+                  <h5>${dish.Price - 50000} vnd</h5>
+                </div>
+              </div>
+            </div>`
+  return content;
 }
 
 function searchDishes(searchText) {
@@ -111,8 +151,121 @@ function searchDishes(searchText) {
       url: url,
       success: function (data) {
         searchedDishes = data;
+        Dishes = [...Dishes, ...data]
+        Dishes = RemoveDuplicate(Dishes)
         renderDishes(1, true);
       }
     });
   }
+}
+
+function AddOrderItem(dishDiv) {
+  id = dishDiv.attr('id')
+  dish = Dishes.find(d => d.ID == id)
+
+  if (newOrderItems.find(t => t.DishID == id)) {
+    newOrderItems = newOrderItems.map( e => e.DishID == id ? { ...e, Quantity: e.Quantity + 1} : e)
+  } else {
+    AddNewItem(dish)
+  }
+  renderOrderItems()
+}
+
+function getDataForm() {
+  Order.TableID = $('#TableID').val()
+}
+
+function AddNewItem(dish) {
+  newItem = {
+    DishID: dish.ID,
+    Quantity: 1,
+    UnitPrice: dish.Price,
+    Discount: 50000,
+  }
+  newOrderItems.push(newItem)
+}
+
+function RemoveNewItem(id) {
+  item = newOrderItems.find( e => e.DishID == id)
+  newOrderItems = remove(newOrderItems, item)
+}
+
+function remove(array, element) {
+    return array.filter(e => e !== element);
+}
+
+function renderOrderItems() {
+  content = `<div class="row border-bottom">
+                <div class="col-sm-3 col-md-3">
+                  <p>Món ăn</p>
+                </div>
+                <div class="col-sm-2 col-md-2">
+                  <p>Giá</p>
+                </div>
+                <div class="col-sm-1 col-md-1">
+                  <p>SL</p>
+                </div>
+                <div class="col-sm-3 col-md-3">
+                  <p>Thành Tiền</p>
+                </div>
+                <divclass="col-sm-2 col-md-2">
+                  <p>Action</p>
+                </div>
+              </div>`
+  newOrderItems.map( item => content += renderItem(item))
+
+  $('.orderItems').html(content)
+}
+
+function renderItem(item) {
+  dish = Dishes.find(d => d.ID == item.DishID)
+  realMoney = item.UnitPrice - item.Discount
+  return content = `<div class="row border-bottom">
+                      <div class="col-sm-3 col-md-3">
+                        <p>${dish.Name}</p>
+                      </div>
+                      <div class="col-sm-2 col-md-2">
+                        <p>${realMoney}</p>
+                      </div>
+                      <div class="col-sm-1 col-md-1">
+                        <p>${item.Quantity}</p>
+                      </div>
+                      <div class="col-sm-3 col-md-3">
+                        <p>${item.Quantity * realMoney}</p>
+
+                      </div>
+                      <divclass="col-sm-2 col-md-2">
+                        <button type="button" class="btn-primary btn-xs" onclick="increaseOrderItem(${item.DishID})">
+                          <span class="glyphicon glyphicon-plus"></span>
+                        </button>
+                        <button type="button" class="btn-warning btn-xs" onclick="dereaseOrderItem(${item.DishID})">
+                          <span class="glyphicon glyphicon-minus"></span>
+                        </button>
+                        <button type="button" class="btn-danger btn-xs" onclick="RemoveOrderItem(${item.DishID})">
+                          <span class="glyphicon glyphicon-remove"></span>
+                        </button>
+                      </div>
+                    </div>`
+}
+
+function increaseOrderItem(DishID){
+  newOrderItems = newOrderItems.map( e => e.DishID == DishID ? { ...e, Quantity: e.Quantity + 1} : e)
+  renderOrderItems()
+}
+
+function dereaseOrderItem(DishID){
+  newOrderItems = newOrderItems.map( e => (e.DishID == DishID && e.Quantity >= 1) ? { ...e, Quantity: e.Quantity - 1} : e)
+  renderOrderItems()
+}
+
+function RemoveOrderItem(DishID) {
+  item = newOrderItems.find( e => e.DishID == DishID)
+  newOrderItems = remove(newOrderItems, item)
+  renderOrderItems()
+}
+
+
+function RemoveDuplicate(arr) {
+  uniq = new Set(arr.map(e => JSON.stringify(e)));
+  return res = Array.from(uniq).map(e => JSON.parse(e));
 }
