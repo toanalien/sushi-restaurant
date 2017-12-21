3c8ef31696b2bb654883de0461eda314d8545329
LinhNguyen0611 <annienguyen.it@gmail.com>
2017-12-21 22:50:21 +0700

    update promotion lan 2

--- Data/Model/Entities/Model.edmx.sql
+++ Data/Model/Entities/Model.edmx.sql
@@ -392,24 +392,6 @@ CREATE TABLE [dbo].[Promotion](
 ) ON [PRIMARY]
 END
 GO
-/****** Object:  Table [dbo].[PromotionDish]    Script Date: 12/21/2017 7:31:33 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PromotionDish]') AND type in (N'U'))
-BEGIN
-CREATE TABLE [dbo].[PromotionDish](
-	[PromotionId] [int] NOT NULL,
-	[DishId] [int] NOT NULL,
- CONSTRAINT [PK_PromotionDish] PRIMARY KEY CLUSTERED 
-(
-	[PromotionId] ASC,
-	[DishId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-END
-GO
 /****** Object:  Table [dbo].[SubCategory]    Script Date: 12/21/2017 7:31:33 PM ******/
 SET ANSI_NULLS ON
 GO
