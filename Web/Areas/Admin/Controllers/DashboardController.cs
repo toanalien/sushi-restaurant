f654f26ce9f1dd8475b606c03dbc0fbb93e5e162
Thiên Toán <toanalien@gmail.com>
2017-12-28 13:55:46 +0700

    WIP chartjs

--- Web/Areas/Admin/Controllers/DashboardController.cs
+++ Web/Areas/Admin/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data.Model.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,12 +8,31 @@ using System.Web.Mvc;
 namespace Web.Areas.Admin.Controllers
 {
     [Authorize]
+
     public class DashboardController : Controller
     {
+        private Entities db = new Entities();
         // GET: Admin
         public ActionResult Index()
         {
             return View();
+        }
+
+        //GET: /Admin/dashboard/data
+
+        public JsonResult GetData()
+        {
+            var countDish = db.Dishes.Count();
+            var countPromotion = db.Promotions.Count();
+
+            var countBanTrong = db.Tables.Where(t => t.Status == 0).Count();
+
+            var data = Json(new
+            {
+                countDish = countDish,
+                countPromotion = countPromotion
+            }, JsonRequestBehavior.AllowGet);
+            return data;
         }
     }
 }
